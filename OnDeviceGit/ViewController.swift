e46c72d7417fcdd91c83f4ddcaf838e68cd21df1
shafiqpatel <shafiqpatel@gmail.com>
2022-09-12 15:17:47 +0530

    first commit

---
+++ OnDeviceGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnDeviceGit
+//
+//  Created by MD SHAFIQ PATEL on 12/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
