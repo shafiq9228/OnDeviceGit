4b9b892adb14dc25d0d0f76d36ea481239b48638
shafiqpatel <shafiqpatel@gmail.com>
2022-09-12 16:13:57 +0530

    tableView, Appicon, image assets

--- OnDeviceGit/ViewController.swift
+++ OnDeviceGit/ViewController.swift
@@ -7,13 +7,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    var listData1 = ["Shafiq", "Silpa", "NagaMani", "Moinuddin", "AndroBim"]
 
+    @IBOutlet weak var MyImage: UIImageView!
+    
+    @IBOutlet weak var tableView1: UITableView!
+    var b1 = true
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        MyImage.contentMode = .scaleToFill
+        
+        tableView1.delegate = self
+        tableView1.dataSource = self
     }
 
-
+    @IBAction func Button1(_ sender: UIButton) {
+        
+        
+        if(b1){
+            
+            MyImage.image = UIImage(named: "abc")
+        } else {
+            MyImage.image = UIImage(named: "download")
+            
+           
+            
+        }
+        
+        
+        b1.toggle()
+      
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return listData1.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath)
+        
+     ///   cell.
+        cell.textLabel?.text = "\(listData1[indexPath.row])"
+        
+    
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(listData1[indexPath.row])")
+    }
+    
 }
 
